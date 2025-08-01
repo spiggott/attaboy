f2253989cd373303a5a60ff14f28a0f6be2dc439
Scott Piggott <scott.piggott@gmail.com>
2025-08-01 14:32:50 -0500

    minor changes

--- attaboy.js
+++ attaboy.js
@@ -1,25 +1,42 @@
 <script>
 (function() {
-  const params = ['utm_source', 'utm_medium', 'utm_campaign', 'utm_term', 'utm_content'];
-  const urlParams = new URLSearchParams(window.location.search);
+  var params = ['utm_source', 'utm_medium', 'utm_campaign', 'utm_term', 'utm_content'];
+  var urlParams = (function() {
+	var result = {};
+	var query = window.location.search.substring(1);
+	var vars = query.split("&");
+	for (var i = 0; i < vars.length; i++) {
+	  var pair = vars[i].split("=");
+	  if (pair.length === 2) {
+		result[decodeURIComponent(pair[0])] = decodeURIComponent(pair[1]);
+	  }
+	}
+	return result;
+  })();
 
   // Store UTM params in localStorage
-  params.forEach(param => {
-	const value = urlParams.get(param);
+  for (var i = 0; i < params.length; i++) {
+	var param = params[i];
+	var value = urlParams[param];
 	if (value) {
-	  localStorage.setItem(param, value);
+	  try {
+		localStorage.setItem(param, value);
+	  } catch (e) {
+		// fail silently
+	  }
 	}
-  });
+  }
 
   // Populate hidden fields on DOM ready
   document.addEventListener('DOMContentLoaded', function() {
-	params.forEach(param => {
-	  const field = document.querySelector(`input[name="${param}"]`);
-	  const storedValue = localStorage.getItem(param);
-	  if (field && storedValue) {
-		field.value = storedValue;
+	for (var j = 0; j < params.length; j++) {
+	  var p = params[j];
+	  var stored = localStorage.getItem(p);
+	  var field = document.querySelector('input[name="' + p + '"]');
+	  if (field && stored) {
+		field.value = stored;
 	  }
-	});
+	}
   });
 })();
 </script>
